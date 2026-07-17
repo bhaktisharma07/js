5efe704afc15ff33fbf18c5f2522c98520453ebe
bhaktisharma07 <bhaktisharma0727@gmail.com>
2026-07-17 12:52:31 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,42 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // alert works in the browser, not in Node.js
+
+//console.log(3 + 3) ; console.log("bhakti") // here code redability is very bad
+
+console.log(3 + 3)
+console.log ("bhakti") // better code redability , code redability should always be high
+
+
+let name = "bhakti"      // String value
+let age = 20             // Number value
+let isLoggedIn = false   // Boolean value
+let state;               // Value is undefined
+
+// Number => Used to store numeric values It has a limit of up to 2^53
+// BigInt => Used to store very large numbers
+// String => Used to store text
+// Boolean => true or false
+// Null => Represents an empty value
+// Undefined => A variable is declared but no value is assigned
+// Symbol => Used to create unique values
+// Object => Used to store a collection of data
+
+
+console.log(typeof "bhakti"); // typeof is used to check the data type of a value.
+console.log(typeof undefined); //undefined
+console.log(typeof null); // Output: "object" (This is a JavaScript bug)
+
+/*
+Summary:
+
+- Number stores numbers
+- BigInt stores very large numbers
+- String stores text
+- Boolean stores true or false
+- Null means empty value
+- Undefined means no value is assigned
+- Symbol creates unique values
+- Object stores a collection of data
+- typeof checks the data type
+*/
