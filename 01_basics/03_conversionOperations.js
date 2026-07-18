fe38e04b06b6b9d36279c5e4e4c6d98a9622c51d
bhaktisharma07 <bhaktisharma0727@gmail.com>
2026-07-18 13:19:17 +0000

    03_conversionOperations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,5 @@
+//************** CONVERSIONS **************//
+
 let score = "27abc" 
 /*
 score is a string.
@@ -10,21 +12,22 @@ So JS returns NaN (Not a Number).
 // let score = undefined // output is NaN
 // let score = "bhakti" // output is NaN
 // let score = 27 // will show type as number
-// let score = "27" // will show type as tring
+// let score = "27" // String value
 
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score) // Converts score into a Number
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+let valueInNumber = Number(score) // Converts score to a number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-// true => 1 
-// false => 0
+// Number(true) = 1
+// Number(false) = 0
+
 // Boolean() converts a value into true or false.
 
-// let isLoggedIn = 1 
+let isLoggedIn = 1 
 // let isLoggedIn = "" // false
 // let isLoggedIn = "bhakti" // true
 
@@ -34,4 +37,63 @@ console.log(valueInNumber);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+//console.log(stringNumber);
+//console .log(typeof stringNumber);
+
+
+
+//************** OPERATIONS **************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);  // Basic arithmetic operators
+
+let str1 = "hello"
+let str2 = " bhakti"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // output = 12
+// console.log(1 + "2");  // output = 12
+// console.log("1" + 2 + 2); // output = 122
+
+// console.log(1 + 2 + "2"); 
+
+// Output = "32"
+// 1 + 2 is calculated first = 3
+// Then 3 + "2" becomes "32"
+
+//console.log( (3 + 4) * 5 % 3);
+
+// If a string comes first, the rest are joined as strings
+// If the string comes last, numbers are added first, then converted to a string
+
+// console.log(+true); // output = 1
+
+// console.log(+""); // output = 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); // output = 101
+
+/*
+Prefix (++x): Value is increased first, then used
+Postfix (x++): Current value is used first, then increased
+
+Read more: MDN Increment (++) Operator
+*/
+
+
+
